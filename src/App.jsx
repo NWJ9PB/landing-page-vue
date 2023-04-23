d6e3c95ec2cea87bfc03536f0191f05dc6bd2e5f
NWJ9PB <helodibalado@gmail.com>
2023-04-23 15:22:12 +0800

    Chore: Change placeholderts

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <h1 className="text-3xl font-bold underline text-emerald-600">
-      Hello world!
-      </h1>
+    <h1 className="text-3xl font-bold mb-6 text-emerald-600">
+      NeXT Systems
+    </h1>
+
+    <h2 className="text-4xl font-extrabold m-8 text-blue-500 animate-pulse">
+      Under Maintenance!
+    </h2>
+
+
+    <p className="text-xl font-semibold text-cyan-600">
+      In the meantime, look at my current progress using new technologies
+    </p>
     </>
   )
 }
