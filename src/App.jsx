690d694aa7f402265b7080ab6852dd1a1e2bd80a
NWJ9PB <69032826+NWJ9PB@users.noreply.github.com>
2024-09-29 00:48:19 +0000

    Fix: Main Page Styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import './App.css'
 function App() {
   return (
     <>
-    <h1 className="text-3xl font-bold mb-6 text-emerald-600">
+    <span className="text-7xl font-bold mb-8 text-emerald-600">
       NeXT Systems
-    </h1>
+    </span>
 
-    <h2 className="text-4xl font-extrabold mb-16 text-red-500 animate-pulse">
+    <h2 className="text-4xl font-extrabold mb-16 mt-16 text-red-500 animate-pulse">
       Under Maintenance!
     </h2>
-
 
     <p className="text-xl font-semibold text-cyan-600">
       In the meantime, look at my current progress using new technologies
